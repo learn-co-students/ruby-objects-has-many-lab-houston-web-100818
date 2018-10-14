c84f9020c48aaa8d81de167d51c161347657597c
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-14 01:23:43 -0500

    done objects has many

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+
+    attr_accessor :name, :songs
+
+    def initialize(name)
+        self.name = name
+        @songs = []
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        song = Song.new(song_name) #creating a new song with the arg
+        @songs << song
+        song.artist = self         #associating the song and the artist
+    end
+
+    def self.song_count
+        Song.all.length
+    end
+
+
+end
